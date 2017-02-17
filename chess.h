b2a842d3c6ebfded8b5e4c1af8c0cdd20a626abf
Robert Robillard <robillard123@gmail.com>
2017-02-17 10:45:27 -0500

    Test file to try compiling
    Here is a file with udpates I have made to attempt to get the program to
    run.

--- chess.h
+++ chess.h
@@ -1,14 +1,17 @@
 //chess.h
+#include <iostream>
 
+using namespace std;
 #include "chessPiece.h"
 
 //Class that contains the "board" which holds all the chessPiece objects
 class chess {
 
-private: 
+private:
 	chessPiece chessPieceArray[8][8];
 
 public:
-
-
+	chess(){
+	      cout << "In the Constructor\n";
+	    }
 };

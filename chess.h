abc11480a9589cfb3ebac10a9f265b55a67b7072
Robert Robillard <robillard123@gmail.com>
2017-02-21 12:40:10 -0500

    Newest Updates To Chess

--- chess.h
+++ chess.h
@@ -8,15 +8,17 @@ using namespace std;
 class chess {
 
 private:
-	chessPiece chessPieceArray[8][8];
+	chessPiece* chessPieceArray[8][8];
 
 public:
 	chess(){
-	      for(int i = 0; i < 8; i++){
-					for(int j = 0; j < 8; j++){
-						chessPieceArray[i][j] = new Pawn('W');
-						break;
-					}
+	    for(int i = 0; i < 8; i++){
+				for(int j = 0; j < 8; j++){
+					chessPieceArray[i][j] = new Pawn('W');
+					cout<< chessPieceArray[i][j]->get_pieceColor() << " ";
 				}
-	    }
+				cout << endl;
+			}
+	 }
+
 };

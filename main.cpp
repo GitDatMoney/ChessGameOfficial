63d6167f3a7eb08e549aec84ac3d41e18187c3af
Robert Robillard <robillard123@gmail.com>
2017-02-22 16:50:39 -0500

    Update to fix failed Upload

--- main.cpp
+++ main.cpp
@@ -7,7 +7,63 @@ using namespace std;
 #include <iostream>
 
 using namespace std;
+
 int main() {
-	chess theBoard;
+
+	int selection = 0;
+	cout << "Welcome To Chess" << endl;
+	bool toBreak = false;
+	chess* theBoard = new chess();
+
+	while(true) {
+		cout << "Please Select A Menu Option: " << endl;
+		cout << "1) Quick Play\n2) Custom Match\n3) Tutorials\n4) Options\n5) Exit" << endl;
+		cin >> selection;
+		int x1,x2,y1,y2;
+
+		switch(selection) {
+			case 1:
+				cout << "Beginning Game: \n\n" << endl;
+				theBoard->setupBoard();
+				cout << "Inital Row: ";
+				cin >> y1;
+				cout << "Inital Column: ";
+				cin >> x1;
+				cout << "Ending Row: ";
+				cin >> y2;
+				cout << "Ending Column: ";
+				cin >> x2;
+					//x1,y1 are the position the user wants to move a piece from,
+					//X2,y2 are the position the user wants to move a piece to,
+
+				//I need you to find a way to make the following line of code work, change it what way you need
+				// and leave LOTS of comments on what you did.
+				
+				//theBoard->chessPieceArray[x1][y1]->pieceMove(x2,y2, theBoard->chessPieceArray);
+			break;
+
+			case 2:
+			break;
+
+			case 3:
+			break;
+
+			case 4:
+			break;
+
+			case 5:
+			//Exit the Program
+				return(0);
+			default:
+			//The user entered an invalid option
+			cout << "Invalid Option, Please make a Valid Selection" << endl;
+			break;
+		}
+	}
+
+
+	cout << selection;
+
+
 	return 0;
 }

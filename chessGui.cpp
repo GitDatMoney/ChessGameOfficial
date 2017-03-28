b5d62b5613eb7f6b1ae30fb051925ea33e445c27
Robert Robillard <robillard123@gmail.com>
2017-03-28 13:02:30 -0400

    Made functioning console GUI

--- chessGui.cpp
+++ chessGui.cpp
@@ -6,37 +6,44 @@ class ChessGUI{
 private:
 public:
   ChessGUI();
-  void printChessGUI();
+  void printChessGUI(chessPiece** myBoardToDraw);
   void welcomeToChess();
   void printBorder();
 };
 ChessGUI::ChessGUI() {
   welcomeToChess();
 }
-void ChessGUI::printChessGUI() {
-  bool printStar = false;
+void ChessGUI::printChessGUI(chessPiece** myBoardToDraw) {
 
+  bool printStar = false;
   /*
     Generate ASCII Text Chess Board,
     Load Piece Color/Type for pieces on the board
   */
-  string myArray[8][8];
-  for(int ii=0; ii < 8; ii++) {
-    for(int jj=0; jj < 8; jj++) {
-      if(ii == 1) {
-        myArray[ii][jj] = "p";
-      } else if(ii == 6) {
-        myArray[ii][jj] = "p";
-      } else {
-        myArray[ii][jj] = " ";
-      }
+  int num = 0;
+  char lettersToUse[8] = {'A','B','C','D','E','F','G','H'};
+  cout << "\t     ";
+  for(int jj=0; jj < 64; jj++) {
+    if(jj%8 == 3) {
+      cout << lettersToUse[num];
+      num++;
+    } else {
+      cout << " ";
     }
   }
+  cout <<"\n";
+
   cout << "\t";
   printBorder();
   cout << "\n";
 
+  int row = 0, col = 0;
+  num = 1;
   for(int jj=1; jj<41; jj++) {
+    if(jj % 5 == 3) {
+      cout << "   " << num;
+      num++;
+    }
     cout << "\t|    ";
     for(int ii=0; ii<64; ii++) {
       if(ii%8 == 0) {
@@ -56,7 +63,16 @@ void ChessGUI::printChessGUI() {
             break;
           case 3:
           //Character Color
-           cout <<"W";
+           if(myBoardToDraw[row][col].get_pieceColor() != '*') {
+             cout << myBoardToDraw[row][col].get_pieceColor();
+           } else {
+             if(printStar) {
+
+               cout << "*";
+             } else {
+               cout << " ";
+             }
+           }
           break;
           default:
           if(printStar) {
@@ -81,7 +97,21 @@ void ChessGUI::printChessGUI() {
             break;
           case 3:
           //Character Type
-           cout <<"p";
+          if(myBoardToDraw[row][col].get_pieceType() != '*') {
+            cout << myBoardToDraw[row][col].get_pieceType();
+          } else {
+            if(printStar) {
+
+              cout << "*";
+            } else {
+              cout << " ";
+            }
+          }
+          col++;
+          if(col > 7){
+            col = 0;
+            row++;
+          }
           break;
           default:
           if(printStar) {
